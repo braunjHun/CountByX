0a80e65ea09ee6b51e942c84d3431e56b633b5db
braunjHun <braunjozsefsoftware@gmail.com>
2021-07-16 09:20:03 +0200

    2. Pomodoro - Refactor

--- src/test.js
+++ src/test.js
@@ -1,11 +1,11 @@
 const { CountBy } = require('./countBy');
 
 describe('Test Count by X functions', () => {
-  it('Test 1x1=1', () => {
+  it('Basic multiplication', () => {
     const countBy = new CountBy();
     expect(countBy.countBy(1,1)).toStrictEqual([1]);
   });
-  it('Multiple 1 by 10 times', () => {
+  it('Multiplication ten times', () => {
     const countBy = new CountBy();
     expect(countBy.countBy(1,10)).toStrictEqual([1,2,3,4,5,6,7,8,9,10]);
   });
