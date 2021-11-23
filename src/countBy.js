c0b8b4d6ed7832a4046fa66faedda1005c735d94
braunjHun <braunjozsefsoftware@gmail.com>
2021-11-23 13:47:50 +0100

    test sonar coud

--- src/countBy.js
+++ src/countBy.js
@@ -1,21 +1,18 @@
 class CountBy {
-    
     countBy(multiplicand, times) {
         var returnArray = [];
-
-        for (let i=0;i<times;i++){
-          var multiplier = i+1;
-          returnArray[i]=this.multiply(multiplier,multiplicand);
+        for (let i = 0; i < times; i++) {
+            var multiplier = i + 1;
+            returnArray[i] = this.multiply(multiplier, multiplicand);
         }
-        
         return returnArray;
     }
 
     multiply(multiplier, multiplicand) {
-        return multiplier*multiplicand;
+        return multiplier * multiplicand;
     }
 }
-  
+
 module.exports = {
     CountBy,
 };
